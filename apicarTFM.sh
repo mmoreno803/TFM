3268ffedd6ed87fd85c608efdab7208e0269b1ed
Miguel Moreno <mmoreno803@gmail.com>
2016-05-22 11:45:50 +0200

    Add files via upload

---
+++ apicarTFM.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/mmoreno803/TFM.git
+cp TFM_files/avahi/* /var/local/cDistro/plug/avahi/
+cp TFM_files/php/* /var/local/cDistro/plug/controllers/
+cp TFM_files/menus/* /var/local/cDistro/plug/menus/
+mkdir /var/local/cDistro/plug/resources/docker
+mkdir /var/local/cDistro/plug/resources/docker/containers
+cp TFM_files/scripts/* /var/local/cDistro/plug/resources/docker/
+cp TFM_files/config/* /var/local/cDistro/plug/resources/docker/containers/
